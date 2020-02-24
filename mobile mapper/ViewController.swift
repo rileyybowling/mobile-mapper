b86ce3e81d9a8a9e46254c7942ec015746497478
Riley Bowling <rbowling@bsd220.org>
2020-02-24 10:32:03 -0600

    Initial Commit

---
+++ mobile mapper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mobile mapper
+//
+//  Created by Riley Bowling on 2/24/20.
+//  Copyright © 2020 Riley Bowling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
